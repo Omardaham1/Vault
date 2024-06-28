881a403c924f0b9132d9a52c582f675e6b2a219f
Omardaham1 <omardaham1@hotmail.com>
2024-06-28 13:07:21 -0400

    Vault

---
+++ index.js
@@ -0,0 +1,14 @@
+// create your first alert which notfies the vister that he has been selected for the code.
+
+alert(`You have received this message because you have been chosen to open an important vault. Here is the secret combination:`);
+
+// create 2nd alert telling vistor the combination 
+
+alert(`Please make sure to save the combination; 10 - 40 - 39.`)
+
+
+// Then you want to create your variables via use of calcualtions
+
+const Combo1 = 55 + 60 - 10 - 95;
+const Combo2 = 2 * 10 + 30 - 10;
+const Combo3 = 10 / 2 * 5 + 14;
